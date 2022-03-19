eacc739f5acbd0f90d50c210212e14f1e3f41863
Ibragim0v <ibragimov.amirshoh@mail.ru>
2022-03-19 15:38:03 +0500

    Add Search Step

--- js/main.js
+++ js/main.js
@@ -1,67 +1,82 @@
-var elList = document.querySelector("#cinema-list")
+// html defaults
+let elList = document.querySelector("#cinema-list")
+let elForm = document.querySelector("#main-form")
+let elInput = document.querySelector("#main-input")
+let inputValue = elInput.value.trim()
+let elSpan = document.querySelector("#main-result")
 
-var newMovieList = movies.slice(0, 10)
+elForm.addEventListener("submit", (evt)=>{
+    evt.preventDefault();
+    
+    let newMovies = movies.filter(function (item) {
+        return item.Title == elInput.value
+    })
+    
+    function renderMovies(array, place) {
+        let number = 1;
+        elList.innerHTML = null;
+        for (const item of newMovies) {
+            elSpan.textContent = number++;
+    
+            let newLi = document.createElement("li");
+            newLi.setAttribute("class", "col-5 mb-4")
+            
+            let newDiv = document.createElement("div")
+            newDiv.setAttribute("class", "card")
+            
+            let newIMG = document.createElement("img")
+            newIMG.setAttribute("src", `https://i.ytimg.com/vi/${item.ytid}/mqdefault.jpg`)
+            newIMG.setAttribute("class", "card-img-top")
+            newIMG.setAttribute("alt", `Movie img`)
+            
+            let newHeading = document.createElement("h4")
+            newHeading.classList.add("card-title")
+            newHeading.textContent = item.Title
 
-function renderMovies(array, place) {
-    elList.innerHTML = null;
-    for (const item of newMovieList) {
-        var newLi = document.createElement("li");
-        newLi.setAttribute("class", "col-5 mb-4")
-        
-        var newDiv = document.createElement("div")
-        newDiv.setAttribute("class", "card")
-        
-        var newIMG = document.createElement("img")
-        newIMG.setAttribute("src", `https://i.ytimg.com/vi/${item.ytid}/mqdefault.jpg`)
-        newIMG.setAttribute("class", "card-img-top")
-        newIMG.setAttribute("alt", `Movie img`)
-        
-        var newHeading = document.createElement("h4")
-        newHeading.classList.add("card-title")
-        newHeading.textContent = item.Title
-        
-        var newPY = document.createElement("p")
-        newPY.setAttribute("class", "card-text fs-5 d-flex align-items-center")
-        newPY.textContent = item.movie_year;
-        
-        var newPR = document.createElement("p")
-        newPR.setAttribute("class", "card-text fs-5 d-flex align-items-center")
-        newPR.textContent = item.imdb_rating;
-        
-        var newInnerDiv = document.createElement("div")
-        newInnerDiv.classList.add("card-body")
-        newInnerDiv.append(newHeading);
-        newInnerDiv.append(newPY);
-        newInnerDiv.append(newPR);
+            let newPY = document.createElement("p")
+            newPY.setAttribute("class", "card-text fs-5 d-flex align-items-center")
+            newPY.textContent = item.movie_year;
+            
+            let newPR = document.createElement("p")
+            newPR.setAttribute("class", "card-text fs-5 d-flex align-items-center")
+            newPR.textContent = item.imdb_rating;
+            
+            let newInnerDiv = document.createElement("div")
+            newInnerDiv.classList.add("card-body")
+            newInnerDiv.append(newHeading);
+            newInnerDiv.append(newPY);
+            newInnerDiv.append(newPR);
+    
+            let newBtnGroup = document.createElement("div")
+            newBtnGroup.setAttribute("class", "d-flex gap-1 justify-content-center mb-3")
 
-        var newBtnGroup = document.createElement("div")
-        newBtnGroup.setAttribute("class", "d-flex justify-content-center mb-3")
-        var newBtnOne = document.createElement("a")
-        newBtnOne.setAttribute("class", "btn btn-outline-primary")
-        newBtnOne.textContent = "Watch Trailer";
-        newBtnOne.setAttribute("href", `https://www.youtube.com/watch?v=${item.ytid}`)
-
-        var newBtnTwo = document.createElement("a")
-        newBtnTwo.textContent = "Watch Film";
-
-        newBtnTwo.setAttribute("class", "btn btn-outline-danger")
-        var newBtnThree = document.createElement("a")
-        newBtnThree.textContent = "Buy Film";
-
-        newBtnThree.setAttribute("class", "btn btn-outline-warning")
-        newBtnGroup.appendChild(newBtnOne)
-        newBtnGroup.appendChild(newBtnTwo)
-        newBtnGroup.appendChild(newBtnThree)
-        
-        
-        
-        newDiv.appendChild(newIMG)
-        newDiv.appendChild(newInnerDiv)
-        newDiv.appendChild(newBtnGroup)
-        
-        newLi.appendChild(newDiv)
-        elList.appendChild(newLi)
+            let newBtnOne = document.createElement("a")
+            newBtnOne.setAttribute("class", "btn btn-outline-primary")
+            newBtnOne.textContent = "Watch Trailer";
+            newBtnOne.setAttribute("href", `https://www.youtube.com/watch?v=${item.ytid}`)
+    
+            let newBtnTwo = document.createElement("a")
+            newBtnTwo.textContent = "More Info";
+            newBtnTwo.setAttribute("class", "btn btn-outline-danger")
+    
+            let newBtnThree = document.createElement("a")
+            newBtnThree.textContent = "Bookmarked";
+            newBtnThree.setAttribute("class", "btn btn-outline-warning")
+    
+            newBtnGroup.appendChild(newBtnOne)
+            newBtnGroup.appendChild(newBtnTwo)
+            newBtnGroup.appendChild(newBtnThree)
+            
+            
+            
+            newDiv.appendChild(newIMG)
+            newDiv.appendChild(newInnerDiv)
+            newDiv.appendChild(newBtnGroup)
+            
+            newLi.appendChild(newDiv)
+            place.appendChild(newLi)
+        }
     }
-}
-
-renderMovies(newMovieList, elList)
+    
+    renderMovies(newMovies, elList)
+})
